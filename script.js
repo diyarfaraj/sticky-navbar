d9508413d4527bf51557c7af278cc5c14af8e077
Diyar Faraj <48604250+diyarfaraj@users.noreply.github.com>
2019-06-29 20:56:26 +0200

    update

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 const nav = document.querySelector("#main");
+const topOfNav = nav.offsetTop;
+
+function fixNav() {
+  console.log(topOfNav, window.scrollY);
+}
+
+window.addEventListener("scroll", fixNav);
